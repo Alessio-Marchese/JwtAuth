559b82d3101405e4159e4be81a043c773d2b380d
Alessio Marchese <alessio.marchese@inflead.com>
2025-04-26 14:07:58 +0200

    Added UserRepository

---
+++ JwtAuth/Repositories/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using JwtAuth.Context;
+using JwtAuth.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtAuth.Repositories;
+
+
+public interface IUserRepository
+{
+    Task<User?> GetByEmailAsync(string email);
+    Task CreateAsync(User user);
+
+}
+public class UserRepository : IUserRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public UserRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User?> GetByEmailAsync(string email)
+        => await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+    public async Task CreateAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+        await _context.SaveChangesAsync();
+    }
+}
